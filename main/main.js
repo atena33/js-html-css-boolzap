9d7812e7f7e94f5ae3be96c96c096359ab7e1272
Anna <annamura833@gmail.com>
2020-04-07 21:06:51 +0200

    Prima milestone

--- main/main.js
+++ main/main.js
@@ -1,1 +1,15 @@
-console.log('ciao');
+// Aggiunta di un messaggio: l’utente scrive
+// un testo nella parte bassa e cliccando invia
+// il testo viene aggiunto al thread sopra, come messaggio verde
+$ (document).ready(function(){
+  $ ('.invia').click(
+    function(){
+      // Salvo l'input dell'utente
+      var utente = $ ('#msg').val();
+
+
+      $ ('.flotta').append('<div class="inviato">' + utente + '</div>');
+    }
+  )
+  }
+);
