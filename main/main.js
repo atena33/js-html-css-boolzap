3b7bcf3cd7f4be3d631b2989f40c8083891e1db4
Anna <annamura833@gmail.com>
2020-04-08 12:15:23 +0200

    Messaggio di risposta

--- main/main.js
+++ main/main.js
@@ -7,14 +7,22 @@ $ (document).ready(function(){
   var ora = d.getHours();
   var minuti = d.getMinutes();
   var orario = ora + ':' + minuti;
-  // $ (orario).addClass('orario');
+
   $ ('.invia').click(
     function(){
       // Salvo l'input dell'utente
       var utente = $ ('#msg').val();
 
       $ ('.flotta').append('<div class="inviato">' + utente + '<p class = "orario">' + orario + '</p>'+ '</div>');
+      $('#msg').val("");
 
+      // Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
+      //  l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+      setTimeout(risposta, 1000);
+      function risposta() {
+        $('.over').append('<div class="ricevuto">' + 'ok' + '<p class = "orario">' + orario + '</p>'+ '</div>');
+      }
     }
   )
   }
