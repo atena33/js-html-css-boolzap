a69d80bbbf49b6822966081f87aa5daeab66e9e0
Anna <annamura833@gmail.com>
2020-04-08 08:55:23 +0200

    Rivisto

--- main/main.js
+++ main/main.js
@@ -2,13 +2,19 @@
 // un testo nella parte bassa e cliccando invia
 // il testo viene aggiunto al thread sopra, come messaggio verde
 $ (document).ready(function(){
+
+  var d = new Date();
+  var ora = d.getHours();
+  var minuti = d.getMinutes();
+  var orario = ora + ':' + minuti;
+  // $ (orario).addClass('orario');
   $ ('.invia').click(
     function(){
       // Salvo l'input dell'utente
       var utente = $ ('#msg').val();
 
+      $ ('.flotta').append('<div class="inviato">' + utente + '<p class = "orario">' + orario + '</p>'+ '</div>');
 
-      $ ('.flotta').append('<div class="inviato">' + utente + '</div>');
     }
   )
   }
